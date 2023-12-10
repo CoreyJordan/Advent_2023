32c782b4b2a5398a654ee9a8e35463c25e4a4b10
Corey Jordan <113132164+CoreyJordan@user.noreply.github.com>
2023-12-10 18:43:09 -0500

    Add OasisReader to interpret inputs

--- src/mirage/Oasis.java
+++ src/mirage/Oasis.java
@@ -2,6 +2,6 @@ package mirage;
 
 public class Oasis {
     public static void main(String[] args) {
-
+        OasisReader reader = new OasisReader("inputs/readings.txt");
     }
 }
---
+++ src/mirage/OasisReader.java
@@ -0,0 +1,8 @@
+package mirage;
+
+public class OasisReader extends data.FileReader {
+    public OasisReader(String filePath) {
+        super(filePath);
+    }
+
+}
