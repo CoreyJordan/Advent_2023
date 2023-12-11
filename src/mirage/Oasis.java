ff89fd37004d1612e21f0fea9454f847dbafdc30
Corey Jordan <113132164+CoreyJordan@user.noreply.github.com>
2023-12-10 19:54:29 -0500

    Add logic to parse readings from string inputs
    Each string is passed into our sequencing lop and parsed at that
    level. This avoids creatings another list to hold all parsed strings.

--- src/mirage/Oasis.java
+++ src/mirage/Oasis.java
@@ -6,17 +6,6 @@ public class Oasis {
     public static void main(String[] args) {
         OasisReader reader = new OasisReader("inputs/readings.txt");
 
-        ArrayList<Long> testList = new ArrayList<>() {
-            {
-                add((long) 10);
-                add((long) 13);
-                add((long) 16);
-                add((long) 21);
-                add((long) 30);
-                add((long) 45);
-            }
-        };
-
         ArrayList<String> histories = new ArrayList<>() {
             {
                 add("0 3 6 9 12 15");
@@ -25,33 +14,43 @@ public class Oasis {
             }
         };
 
-        // Difference down to zero
-        ArrayList<ArrayList<Long>> sequenceLists = new ArrayList<>();
-        sequenceLists.add(testList);
-        boolean allZeroes = false;
-        while (!allZeroes) {
-            sequenceLists.add(plotDifferences(sequenceLists.getLast()));
+        long extrapolationsTotal = 0;
+        for (int i = 0; i < histories.size(); i++) {
+            // Get readings from the history and add to list of sequences
+            ArrayList<Long> history = reader.findReadings(histories.get(i));
+            ArrayList<ArrayList<Long>> sequenceLists = new ArrayList<>();
+            sequenceLists.add(history);
 
-            if (listSum(sequenceLists.getLast()) == 0) {
-                allZeroes = true;
+            // Difference down to zero
+            boolean allZeroes = false;
+            while (!allZeroes) {
+                sequenceLists.add(plotDifferences(sequenceLists.getLast()));
+
+                if (listSum(sequenceLists.getLast()) == 0) {
+                    allZeroes = true;
+                }
             }
-        }
 
-        // Initialize the extrapolation
-        sequenceLists.remove(sequenceLists.getLast());
-        long delta = 0;
-
-        // Extrapolate to original list
-        while (sequenceLists.size() > 1) {
-            sequenceLists.getLast().add(sequenceLists.getLast().getLast() + delta);
-            delta = sequenceLists.getLast().getLast();
+            // Initialize the extrapolation
             sequenceLists.remove(sequenceLists.getLast());
+            long delta = 0;
+
+            // Extrapolate to original list
+            while (sequenceLists.size() > 1) {
+                sequenceLists.getLast().add(sequenceLists.getLast().getLast() + delta);
+                delta = sequenceLists.getLast().getLast();
+                sequenceLists.remove(sequenceLists.getLast());
+            }
+            // Final delta
+            long extrapolation = sequenceLists.getLast().getLast() + delta;
+            extrapolationsTotal += extrapolation;
+
+            // Reset the sequence list
+            sequenceLists.clear();
+
         }
-        // Final delta
-        long extrapolation = sequenceLists.getLast().getLast() + delta;
 
-        System.out.println(extrapolation);
-
+        System.out.println("Total of extrapolations: " + extrapolationsTotal);
     }
 
     private static ArrayList<Long> plotDifferences(ArrayList<Long> sequence) {
--- src/mirage/OasisReader.java
+++ src/mirage/OasisReader.java
@@ -1,8 +1,22 @@
 package mirage;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class OasisReader extends data.FileReader {
     public OasisReader(String filePath) {
         super(filePath);
+    }
+
+    public ArrayList<Long> findReadings(String s) {
+        ArrayList<Long> list = new ArrayList<>();
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(s);
+        while (matcher.find()) {
+            list.add(Long.parseLong(matcher.group()));
+        }
+        return list;
     }
 
 }
